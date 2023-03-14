65927941f30bd6093fb9f088feac34b446757d0e
Sergei Galchin <warlik.galchin@yandex.ru>
2023-03-14 15:12:34 +0500

    Optimize functions commit

--- main.py
+++ main.py
@@ -5,27 +5,28 @@ import json
 def get_superhero_dict():
     """
     Function gets all information about all heroes and returns the dictionary consisting of
-    hero names and their ids.
+    hero names and list of hero id and intelligence.
     :return:
     """
     url = f'https://akabab.github.io/superhero-api/api/all.json'
     request = requests.get(url)
     all_superheros = json.loads(request.text)
-    return {hero['name']: hero['id'] for hero in all_superheros}
+    return {hero['name']: [hero['id'], hero['powerstats']['intelligence']] for hero in all_superheros}
 
 
-def get_superhero_params(super_dict, superhero):
-    """
-    Function gets superhero's parameters from the API.
-    :param super_dict:
-    :param superhero:
-    :return:
-    """
-    super_num = super_dict[superhero]
-    url = f'https://akabab.github.io/superhero-api/api/powerstats/{super_num}.json'
-    request = requests.get(url)
-    params = json.loads(request.text)
-    return params
+# def get_superhero_params(super_dict, superhero):
+#     """
+#     Additional function. Not used in exercise.
+#     Function gets superhero's parameters from the API.
+#     :param super_dict:
+#     :param superhero:
+#     :return:
+#     """
+#     super_num = super_dict[superhero]
+#     url = f'https://akabab.github.io/superhero-api/api/powerstats/{super_num}.json'
+#     request = requests.get(url)
+#     params = json.loads(request.text)
+#     return params
 
 
 def get_the_most_intelligent_hero(superhero_list):
@@ -41,9 +42,8 @@ def get_the_most_intelligent_hero(superhero_list):
     sph_dict = get_superhero_dict()
     for name in superhero_list:
         if name in sph_dict:
-            current_hero = get_superhero_params(sph_dict, name)
-            if current_hero['intelligence'] > most_intelligent[1]:
-                most_intelligent = (name, current_hero['intelligence'])
+            if sph_dict[name][1] > most_intelligent[1]:
+                most_intelligent = (name, sph_dict[name][1])
     return most_intelligent[0] if most_intelligent[0] else 'Heroes not found'
 
 
